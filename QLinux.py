68156c5a36dc914858e27950a6132957745add15
itsmanu4u <mohan.2kit@gmail.com>
2018-07-23 17:23:42 +0530

    Script updated, EOI fixed

--- QLinux.py
+++ QLinux.py
@@ -19,35 +19,49 @@ print('''\t "1" To update your System \t"2" Your system info \t "3" Check Rootki
 #program code starts here
 
 import subprocess #for invoking system process
-
+count = 0
 while True:
+	try:
+		inu=int(input('\n\n Enter your number # '))
 	
-	inu=int(input('\n\n Enter your number # '))
-	if inu==1:
-		subprocess.call(["sudo", "apt-get", "update"])  #adding Commands to Invoked System process.
-		continue
-	if inu==2:
-		subprocess.call(["lscpu"]) 
-		continue
-	if inu==3:
-		subprocess.call(["chkrootkit"]) 
-		continue
-	if inu==4:
-		subprocess.call(["lsusb"]) 
-		continue
-	if inu==0:
-		subprocess.call(["clear"]) 
-		subprocess.call(["python3", "QLinux.py"]) 	
-		continue
-	if inu==10:
-		print(' \n\n\t\t Good Bye...\n\n') 	
-		break	
-	else:
-		print('\n\nYou Enterd:',inu,' #Are Trying to be Oversmart Now?, Get Out!!\n\n')
-		break
+		if inu == 1:
+			subprocess.call(["sudo", "apt-get", "update"])  #adding Commands to Invoked System process.
+			continue
+		elif inu == 2:
+			subprocess.call(["lscpu"])
+			continue
+		elif inu == 3:
+			subprocess.call(["chkrootkit"])
+			continue
+		elif inu == 4:
+			subprocess.call(["lserrr"])
+			continue
+		elif inu == 0:
+			subprocess.call(["clear"])
+			subprocess.call(["python3", "QLinux.py"])
+			continue
+		elif inu == 10:
+			print(' \n\n\t\t Good Bye...\n\n')
+			break
+		
+		else:
+			print('\n\nYou Enterd:',inu,' #Are Trying to be Oversmart Now?, Get Out!!\n\n')
+			break
+	except ValueError:
+		
+		print('You Didn\'t Enter Anything, My Friend!! \n')
+		count += 1
+		if count > 2:
+			print('You didn\'t Enter anything',count,'TIMES ???','Nothing can Help YOU Lazy, Get Lost!!')
+			break
+		else:
+			continue
+	except KeyboardInterrupt:
+		print("This Operating (Ctrl+C) is Not Allowed, My Friend")
+	except EOFError:
+		print("This Operating (Ctrl+D) is Not Allowed, My Friend")
 
-
-
+	# End of program, Hope you like it buddy!
 
 
 
